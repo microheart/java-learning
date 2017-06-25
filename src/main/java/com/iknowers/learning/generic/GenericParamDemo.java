c53f6ae835e35f3bc997aa92c90b64dad7227169
Shun Xu <shun.xu@sap.com>
2017-06-25 15:08:27 +0800

    优化例子

--- src/main/java/com/iknowers/learning/generic/GenericParamDemo.java
+++ src/main/java/com/iknowers/learning/generic/GenericParamDemo.java
@@ -4,34 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
  * http://stackoverflow.com/questions/4343202/difference-between-super-t-and-extends-t-in-java
  * PECS
-
- Remember PECS: "Producer Extends, Consumer Super".
-
- "Producer Extends" -
- If you need a List to produce T values (you want to read Ts from the list), you need to declare it
- with ? extends T, e.g. List<? extends Integer>. But you cannot add to this list.
-
- "Consumer Super" - If you need a List to consume T values (you want to write Ts into the list),
- you need to declare it with ? super T, e.g. List<? super Integer>. But there are no guarantees
- what type of object you may read from this list.
-
- If you need to both read from and write to a list, you need to declare it exactly with no wildcards, e.g. List<Integer>.
+ * <p>
+ * Remember PECS: "Producer Extends, Consumer Super".
+ * <p>
+ * "Producer Extends" -
+ * If you need a List to produce T values (you want to read Ts from the list), you need to declare it
+ * with ? extends T, e.g. List<? extends Integer>. But you cannot add to this list.
+ * <p>
+ * "Consumer Super" - If you need a List to consume T values (you want to write Ts into the list),
+ * you need to declare it with ? super T, e.g. List<? super Integer>. But there are no guarantees
+ * what type of object you may read from this list.
+ * <p>
+ * If you need to both read from and write to a list, you need to declare it exactly with no wildcards, e.g. List<Integer>.
  */
 public class GenericParamDemo {
     public static void main(String[] args) {
         List<Cat> cats = new ArrayList<>();
-        cats.add(new Cat());
-        cats.add(new HelloKitty());
-//        cats.add(new Animal());
+        superTest(cats);
+
+        List<HelloKitty> kitties = new ArrayList<>();
+        kitties.add(new HelloKitty());
+        extendsTest(kitties);
     }
 
-    public void extendsTest() {
-        List<? extends Cat> cats = new ArrayList<>();
-        List<? extends Cat> cats2 = new ArrayList<HelloKitty>();
-
+    public static void extendsTest(final List<? extends Cat> cats) {
         // writing
 //        http://stackoverflow.com/questions/4343202/difference-between-super-t-and-extends-t-in-java
 //        You can't add any object to List<? extends T> because you can't guarantee what kind of List it is really pointing to,
@@ -41,19 +39,18 @@ public class GenericParamDemo {
 //        cats.add(new HelloKitty()); // compile error
 
         // reading
-        Cat c = cats.get(0);
-        Cat c2 = cats2.get(0);
+        if (!cats.isEmpty()) {
+            System.out.println(cats.get(0).name());
+        }
     }
 
-    public void superTest() {
-        List<? super Cat> cats = new ArrayList<>();
-
+    public static void superTest(final List<? super Cat> cats) {
         cats.add(new Cat());
         cats.add(new HelloKitty());
 //        cats.add(new Animal());  // compile error
 
 //        Cat c = cats.get(0); // compile error
-        Object obj = cats.get(0);
+        cats.forEach(System.out::println);
     }
 
     static class Animal {
